961c4c41bcb08d5b14bb0c36cea4ba405d0521a5
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-09-22 16:07:08 -0500

    Agregar ejemplo de filtro

--- src/js/app/senales.js
+++ src/js/app/senales.js
@@ -8,28 +8,74 @@
 
 	} );
 
-	var componentSelector = '#js-content-signals';
+	var $componentSelector = $( '#js-content-signals' );
+	var components = window.components || {};
 
-	var senalesComponent = (function() {
+	components.signalsComponent = {};
+
+	var signalsComponent = (function() {
 		var serviceURL = 'http://projects-api.webtraining.zone:4000/traffic-signals/v1/signals';
 		var templateURL = '/dist/templates/signals.hbs';
+		var compiledTemplate = null;
 
 		function init() {
 
 			$.when.apply( this, [ getAJAXDataByURL( templateURL ), getAJAXDataByURL( serviceURL ) ] )
 				.then( function( responseSourceTemplate, responseData ) {
+					// Render the signals
 					var source = responseSourceTemplate[ 0 ];
-					var data = responseData[ 0 ];
-					var compiledTemplate = Handlebars.compile( source );
 
-					var html = compiledTemplate( { title: 'Señales de tránsito', signals: data.signals } );
-					$( componentSelector ).html( html );
+					compiledTemplate = Handlebars.compile( source );
+					components.signalsComponent.originalSignals = responseData[ 0 ].signals;
 
+					// Render the template
+					renderSignals( components.signalsComponent.originalSignals );
+
+					// Create filter
+					createFilter();
 				} );
+		}
+
+		function renderSignals( signals ) {
+			var html = compiledTemplate( { signals: signals } );
+			$componentSelector.html( html );
 		}
 
 		function getAJAXDataByURL( url ) {
 			return $.ajax( url );
+		}
+
+		function filterSignals( searchTerm ) {
+			if ( searchTerm === '' ) return components.signalsComponent.originalSignals;
+
+			return components.signalsComponent.originalSignals.filter( function( signal ) {
+				return signal.name.toLocaleLowerCase().indexOf( searchTerm ) >= 0;
+			} );
+		}
+
+		function createFilter() {
+			function hideNonMatchingItems( $items, filterTerm ) {
+				$items.each( function( i ) {
+					var $this = $( this );
+					var wasTextFound = $this.text().toLowerCase().indexOf( filterTerm ) < 0;
+					$this.toggleClass( 'hidden', wasTextFound );
+				} );
+			}
+
+			var $items = $componentSelector.find( 'div.b-signal' );
+
+			$( '#js-filter' ).keyup( function( e ) {
+				var $this = $( this );
+				//if ( $this.val().trim() !== '' ) {
+				var searchTerm = $this.val().toLowerCase();
+
+				console.log( searchTerm );
+				// hideNonMatchingItems( $items, searchTerm );
+				var filteredSignals = filterSignals( searchTerm );
+				renderSignals( filteredSignals );
+				//}
+
+			} );
 		}
 
 		return {
@@ -37,8 +83,12 @@
 		}
 	})();
 
-	if ( ( componentSelector ).length > 0 ) {
-		senalesComponent.init();
+	if ( $componentSelector.length > 0 ) {
+		signalsComponent.init();
+	}
+
+	if ( typeof window.components === 'undefined' ) {
+		window.components = components;
 	}
 
 })( window.jQuery );
